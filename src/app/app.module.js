af21f03e3fbf7ac6614499106648b90a1bd78942
g00dnatur3 <g00dnatur3@gmail.com>
2017-10-04 04:02:34 -0700

    added nice email format validation with directive & ngMessage

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,19 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
-import './app.css';
+
 import angular from 'angular';
 import home from './home/home.module.js'
 import student from './student/student.module.js'
 
 const app = () => {
   return {
-    template: require('./app.html')
+    template: require('./app.html'),
+    css: require('./app.css')
   }
 };
 
 const MODULE_NAME = 'app';
 
 angular.module(MODULE_NAME, [home, student])
-  .directive('app', app)
+  .directive('app', app);
 
 export default MODULE_NAME;
--- src/app/student/student.module.js
+++ src/app/student/student.module.js
@@ -1,8 +1,13 @@
 import angular from 'angular';
 import ngRoute from 'angular-route';
 import ngMessages from 'angular-messages';
+
+import 'angularjs-datepicker/src/css/angular-datepicker.css';
+import 'angularjs-datepicker'
+
 import routes from './student.routes'
 import StudentService from './student.service'
+
 
 // student controller
 class StudentCtrl {
@@ -17,6 +22,10 @@ class StudentCtrl {
 
 // validate email directive
 const validateEmail = (StudentService) => {
+  function validateFormat(email) {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+  }
   return {
     require:'ngModel',
     restrict:'A',
@@ -25,13 +34,11 @@ const validateEmail = (StudentService) => {
       // https://toddmotto.com/moving-from-ng-model-parsers-to-ng-model-validates-ng-messages/
       ctrl.$validators.format = function(modelValue, viewValue) {
         const val = modelValue || viewValue;
-        //if (val.length > 0) return true;
-        return false;
+        return (!val) ? false : validateFormat(val);
       }
       ctrl.$validators.unique = function(modelValue, viewValue) {
         const val = modelValue || viewValue;
-        //if (val.length > 0) return true;
-        return false;
+        return true;
       }
     }
   }
@@ -39,7 +46,7 @@ const validateEmail = (StudentService) => {
 
 const MODULE_NAME = 'student';
 
-angular.module(MODULE_NAME, [ngRoute, ngMessages])
+angular.module(MODULE_NAME, [ngRoute, ngMessages, '720kb.datepicker'])
   .controller('StudentCtrl', StudentCtrl)
   .config(routes)
   .service('StudentService', StudentService)
